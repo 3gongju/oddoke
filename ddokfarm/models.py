295080e35bca204a218e91530e500b581c7b4f6d
yoojin <euvb918@gmail.com>
2025-05-15 10:35:08 +0900

    Post modeling/migration/admin에 모델 등록

--- ddokfarm/models.py
+++ ddokfarm/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django_resized import ResizedImageField
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    image = ResizedImageField(
+        size=[500, 500],
+        crop=['middle', 'center'],
+        upload_to='image'
+    )
