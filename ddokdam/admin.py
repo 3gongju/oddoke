e3d39068398a5764e4e516440c60be5fcb5d3d54
Sususu <iamsummer614@gmail.com>
2025-05-17 18:09:54 +0900

    ddokdam/admin.py 충돌 해결

--- ddokdam/admin.py
+++ ddokdam/admin.py
@@ -2,8 +2,4 @@ from django.contrib import admin
 from .models import DdokdamPost, DdokdamComment
 
 admin.site.register(DdokdamPost)
-<<<<<<< HEAD
-admin.site.register(DdokdamComment)
-=======
-admin.site.register(DdokdamComment)
->>>>>>> 0fc38c738e3dab5016788bbea9138ef1876252bb
+admin.site.register(DdokdamComment) 
