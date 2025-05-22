9911014126c000fae3cb748284dffe92d6a55e36
yoojin <euvb918@gmail.com>
2025-05-22 13:23:15 +0900

    덕담 생일카페 모델 수정

--- ddokdam/models.py
+++ ddokdam/models.py
@@ -25,15 +25,13 @@ class DamMannerPost(DamPost):
 class DamBdaycafePost(DamPost):
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='liked_bdaycafe_posts', blank=True)
     cafe_name = models.CharField(max_length=255, blank=True, null=True)
-    cafe_location = models.CharField(max_length=255, blank=True, null=True)
-    start_date = models.DateField(blank=True, null=True)
-    end_date = models.DateField(blank=True, null=True)
 
 class DamComment(models.Model):
     content = models.CharField(max_length=200)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies')  # ✅ 대댓글
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     community_post = models.ForeignKey(DamCommunityPost, on_delete=models.CASCADE, null=True, blank=True)
     manner_post = models.ForeignKey(DamMannerPost, on_delete=models.CASCADE, null=True, blank=True)
