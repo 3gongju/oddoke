f9eeb0575ed0546409e19361b0380daa9767d9ae
Sususu <iamsummer614@gmail.com>
2025-05-24 14:38:33 +0900

    mainpage 에서 각 ddokfarm, ddokdam의 최신 게시물 3개 보이기, 누르면 detail로 연결

--- oddoke/views.py
+++ oddoke/views.py
@@ -1,24 +1,24 @@
+# grouped_artists, 배너 이미지, 그리고 6개 카테고리별 게시글 리스트를 전달
 from itertools import zip_longest
 from django.shortcuts import render
 from artist.models import Artist
+from ddokfarm.models import FarmSellPost, FarmRentalPost, FarmSplitPost
+from ddokdam.models import DamCommunityPost, DamMannerPost, DamBdaycafePost
 
 def group_artists(artists, group_size=5):
-    """리스트를 group_size씩 묶고 None 제거"""
     return [list(filter(None, group)) for group in zip_longest(*[iter(artists)] * group_size)]
 
 def main(request):
+    # 찜한 아티스트
     favourite_artists = []
     if request.user.is_authenticated:
         favourite_artists = list(Artist.objects.filter(followers=request.user))
 
-    # ✅ 전체 아티스트 목록 가져오기
     all_artists = Artist.objects.all()
-
-    # ✅ 찜한 아티스트 제외한 나머지
     favourite_ids = [artist.id for artist in favourite_artists if hasattr(artist, 'id')]
     other_artists = all_artists.exclude(id__in=favourite_ids)
 
-    # ✅ 5개 미만이면 dummy 추가
+    # 찜한 아티스트가 5개 미만이면 빈 슬롯 추가
     max_count = 5
     if len(favourite_artists) < max_count:
         empty_slots = max_count - len(favourite_artists)
@@ -31,6 +31,7 @@ def main(request):
 
     grouped_artists = group_artists(favourite_artists)
 
+    # 배너 이미지 경로
     banner_images = [
         'image/banner/banner_basic2.png',
         'image/banner/banner1.jpg',
@@ -39,8 +40,42 @@ def main(request):
         'image/banner/banner4.jpg',
     ]
 
+    # ✅ 덕팜 게시글 (각 게시글에 category 수동 부여)
+    latest_sell_posts = list(FarmSellPost.objects.order_by('-created_at')[:3])
+    for post in latest_sell_posts:
+        post.category = 'sell'
+
+    latest_rental_posts = list(FarmRentalPost.objects.order_by('-created_at')[:3])
+    for post in latest_rental_posts:
+        post.category = 'rental'
+
+    latest_split_posts = list(FarmSplitPost.objects.order_by('-created_at')[:3])
+    for post in latest_split_posts:
+        post.category = 'split'
+
+    # ✅ 덕담 게시글 (각 게시글에 category 수동 부여)
+    latest_community_posts = list(DamCommunityPost.objects.order_by('-created_at')[:3])
+    for post in latest_community_posts:
+        post.category = 'community'
+
+    latest_manner_posts = list(DamMannerPost.objects.order_by('-created_at')[:3])
+    for post in latest_manner_posts:
+        post.category = 'manner'
+
+    latest_bdaycafe_posts = list(DamBdaycafePost.objects.order_by('-created_at')[:3])
+    for post in latest_bdaycafe_posts:
+        post.category = 'bdaycafe'
+
     return render(request, 'main/home.html', {
         'banner_images': banner_images,
         'grouped_artists': grouped_artists,
-        'other_artists': other_artists,  # ✅ 여기를 꼭 추가!
+        'other_artists': other_artists,
+
+        'latest_sell_posts': latest_sell_posts,
+        'latest_rental_posts': latest_rental_posts,
+        'latest_split_posts': latest_split_posts,
+
+        'latest_community_posts': latest_community_posts,
+        'latest_manner_posts': latest_manner_posts,
+        'latest_bdaycafe_posts': latest_bdaycafe_posts,
     })
