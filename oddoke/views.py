bd6d9f3b78f613965fd109c3bc3eb1ba40e67ffa
Sususu <iamsummer614@gmail.com>
2025-05-24 13:02:54 +0900

    mainpage 찜한 아티스트 로고 가운데 정렬, 5개 기준 설정 완료

--- oddoke/views.py
+++ oddoke/views.py
@@ -2,32 +2,32 @@
 
 # def main(request):
 #     return render(request, 'main/home.html')
-
+from itertools import zip_longest
 from django.shortcuts import render
-from artist.models import Artist  # 찜한 아티스트 정보를 위해 필요
+from artist.models import Artist
+  # Artist 모델 import
 
-# def main(request):
-#     favourite_artists = []
-#     if request.user.is_authenticated:
-#         favourite_artists = Artist.objects.filter(followers=request.user)
-
-#     return render(request, 'main/home.html', {
-#         'favourite_artists': favourite_artists,
-#     })
+def group_artists(artists, group_size=5):
+    """리스트를 group_size씩 묶어서 None은 제거"""
+    return [list(filter(None, group)) for group in zip_longest(*[iter(artists)] * group_size)]
 
 def main(request):
     favourite_artists = []
     if request.user.is_authenticated:
         favourite_artists = list(Artist.objects.filter(followers=request.user))
 
+    # ✨ 5개 미만일 경우 빈 로고로 패딩
     max_count = 5
-    empty_slots = max_count - len(favourite_artists)
-    for _ in range(empty_slots):
-        favourite_artists.append({
-            'logo': 'image/ddok_logo.png',
-            'display_name': ''
-        })
+    if len(favourite_artists) < max_count:
+        empty_slots = max_count - len(favourite_artists)
+        for _ in range(empty_slots):
+            favourite_artists.append({
+                'logo': 'image/ddok_logo.png',
+                'display_name': ''
+            })
 
+    # ✅ 5개씩 그룹화
+    grouped_artists = group_artists(favourite_artists)
 
     banner_images = [
         'image/banner/banner_basic2.png',
@@ -39,5 +39,5 @@ def main(request):
 
     return render(request, 'main/home.html', {
         'banner_images': banner_images,
-        'padded_artists': favourite_artists,
+        'grouped_artists': grouped_artists,
     })
