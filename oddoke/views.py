ed31bd4f8872e24f2b16386cf0d9a2497bde2415
Sususu <iamsummer614@gmail.com>
2025-05-24 15:55:48 +0900

    mainpage 찜한 아티스트 0개일때 오리 로고 보이기 수정

--- oddoke/views.py
+++ oddoke/views.py
@@ -1,4 +1,5 @@
 # grouped_artists, 배너 이미지, 그리고 6개 카테고리별 게시글 리스트를 전달
+# main/views.py
 from itertools import zip_longest
 from django.shortcuts import render
 from artist.models import Artist
@@ -6,32 +7,17 @@ from ddokfarm.models import FarmSellPost, FarmRentalPost, FarmSplitPost
 from ddokdam.models import DamCommunityPost, DamMannerPost, DamBdaycafePost
 
 def group_artists(artists, group_size=5):
+    # None 은 제외하고 실제 artist 인스턴스만 모아 5개씩 그룹으로 반환
     return [list(filter(None, group)) for group in zip_longest(*[iter(artists)] * group_size)]
 
 def main(request):
-    # 찜한 아티스트
-    favourite_artists = []
-    if request.user.is_authenticated:
-        favourite_artists = list(Artist.objects.filter(followers=request.user))
+    # 1) 찜한 아티스트 원본 목록
+    raw_favs = list(Artist.objects.filter(followers=request.user)) if request.user.is_authenticated else []
 
-    all_artists = Artist.objects.all()
-    favourite_ids = [artist.id for artist in favourite_artists if hasattr(artist, 'id')]
-    other_artists = all_artists.exclude(id__in=favourite_ids)
+    # 2) 그룹별 페이징 캐러셀을 위한 5개씩 묶기
+    grouped_artists = group_artists(raw_favs) if raw_favs else []
 
-    # 찜한 아티스트가 5개 미만이면 빈 슬롯 추가
-    max_count = 5
-    if len(favourite_artists) < max_count:
-        empty_slots = max_count - len(favourite_artists)
-        for _ in range(empty_slots):
-            favourite_artists.append({
-                'id': None,
-                'logo': 'image/ddok_black.png',
-                'display_name': ''
-            })
-
-    grouped_artists = group_artists(favourite_artists)
-
-    # 배너 이미지 경로
+    # 3) 배너 이미지 리스트
     banner_images = [
         'image/banner/banner_basic2.png',
         'image/banner/banner1.jpg',
@@ -40,7 +26,7 @@ def main(request):
         'image/banner/banner4.jpg',
     ]
 
-    # ✅ 덕팜 게시글 (각 게시글에 category 수동 부여)
+    # 4) 덕팜 최신 게시물 3개 + 카테고리 수동 부여
     latest_sell_posts = list(FarmSellPost.objects.order_by('-created_at')[:3])
     for post in latest_sell_posts:
         post.category = 'sell'
@@ -53,7 +39,7 @@ def main(request):
     for post in latest_split_posts:
         post.category = 'split'
 
-    # ✅ 덕담 게시글 (각 게시글에 category 수동 부여)
+    # 5) 덕담 최신 게시물 3개 + 카테고리 수동 부여
     latest_community_posts = list(DamCommunityPost.objects.order_by('-created_at')[:3])
     for post in latest_community_posts:
         post.category = 'community'
@@ -66,10 +52,11 @@ def main(request):
     for post in latest_bdaycafe_posts:
         post.category = 'bdaycafe'
 
+    # 6) 렌더링
     return render(request, 'main/home.html', {
-        'banner_images': banner_images,
+        'raw_favs': raw_favs,
         'grouped_artists': grouped_artists,
-        'other_artists': other_artists,
+        'banner_images': banner_images,
 
         'latest_sell_posts': latest_sell_posts,
         'latest_rental_posts': latest_rental_posts,
