1abc9fca610d7207ba5f15e35d336e55801a73fc
yoojin <euvb918@gmail.com>
2025-05-26 14:44:38 +0900

    mypage 만드는중...

--- artist/urls.py
+++ artist/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('', views.index, name='index'), #검색 + 찜 분리된 최신 index 뷰
     path('<int:artist_id>/toggle/', views.toggle_favourite, name='toggle_favourite'),
     path('autocomplete/', views.autocomplete, name='autocomplete'),
+
+    # 멤버 리스트 Ajax 요청
+    path('artist/<int:artist_id>/members/', views.artist_members_ajax, name='artist_members_ajax'),
+
+    # 멤버 팔로우 토글 Ajax 요청
+    path('member/<int:member_id>/follow-toggle/', views.follow_member_ajax, name='follow_member_ajax'),
 ]
